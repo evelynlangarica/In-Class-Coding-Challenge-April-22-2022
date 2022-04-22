c5a9e32e86fb5db0781f45229c95467b2e6e12eb
Evelyn Langarica <evelynlangarica@gmail.com>
2022-04-22 13:40:35 -0400

    Commit april 22

---
+++ script.js
@@ -0,0 +1,12 @@
+let condiments = ['Ketchup', 'Salsa', 'Mayo', 'Ranch', 'Mustard'];
+let plates = [1, 2, 3, 4, 5, 6];
+let questions = ['true', 'false', 'yes', 'no', 't', 'f'];
+
+
+
+
+fun1(plates);
+function fun1(plates) {
+    plates.push('7');
+}
+console.log(plates);
